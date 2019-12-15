095cc253c0973d677533e35dae357920444e5ceb
Coin for the future <37978712+cadexproject@users.noreply.github.com>
2019-12-15 21:21:53 +0100

    Update version.h

--- src/version.h
+++ src/version.h
@@ -11,7 +11,7 @@
  */
 
 
-static const int PROTOCOL_VERSION = 70215;
+static const int PROTOCOL_VERSION = 70212;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -20,7 +20,7 @@ static const int INIT_PROTO_VERSION = 209;
 static const int GETHEADERS_VERSION = 70077;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 70210;
+static const int MIN_PEER_PROTO_VERSION = 70208;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
@@ -48,9 +48,10 @@ static const int SHORT_IDS_BLOCKS_VERSION = 70209;
 static const int DMN_PROTO_VERSION = 70213;
 
 //! introduction of LLMQs
-static const int LLMQS_PROTO_VERSION = 70214;
+static const int LLMQS_PROTO_VERSION = 70212;
 
 //! introduction of SENDDSQUEUE
-static const int SENDDSQUEUE_PROTO_VERSION = 70214;
+//! TODO we can remove this in 0.15.0.0
+static const int SENDDSQUEUE_PROTO_VERSION = 70201;
 
 #endif // BITCOIN_VERSION_H
